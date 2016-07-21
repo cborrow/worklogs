7af53824b2efe2b5a0a00932493facb3a9780e74
Cory Borrow <cborrow03@gmail.com>
2016-07-21 19:59:58 -0400

    Add job methods and dashboard method.
    Added a dashboard method for calling dashboard view.
    Add job methods for getting job information for ajax commands.

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Job;
+use App\Status;
+use App\Store;
 use App\Http\Requests;
 
 class JobsController extends Controller
@@ -20,6 +22,12 @@ class JobsController extends Controller
 
     public function edit(Job $job) {
         return view('notes.edit', compact('job'));
+    }
+
+    public function dashboard() {
+        $jobs = Job::all();
+        $stores = Store::all();
+        return view('notes.dashboard', compact('jobs', 'stores'));
     }
 
     public function store(Request $request) {
@@ -64,5 +72,29 @@ class JobsController extends Controller
         $job->save();
 
         return back();
+    }
+
+    public function getJobStatus(Request $request, Job $job) {
+        $status = Job::getStatusName($job->status_id);
+        return $status;
+    }
+
+    public function getStatusList(Request $request) {
+        $list = Status::all();
+        return json_encode($list);
+    }
+
+    public function setJobStatus(Request $request, Job $job) {
+        $val = $request->value;
+
+        if(is_numeric($val)) {
+            $job->status_id = $val;
+            $job->save();
+        }
+    }
+
+    public function apiGetStatus(Request $request, Job $job) {
+        $status = $this->getJobStatus($request, $job);
+        echo $status;
     }
 }

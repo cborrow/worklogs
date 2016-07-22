e71f8700119e4aaea2aecb30159bd96d00feffa6
Cory Borrow <cborrow03@gmail.com>
2016-07-21 20:02:33 -0400

    Changed the way the pop-ups work, they work much better now.

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+var baseURI = 'http://localhost/worklogs/public/index.php/';
+
 function showOverlay() {
     var popup = $('.popup-overlay');
     popup.css('display', 'block');
@@ -13,26 +15,44 @@ function showPassword(str) {
 function changeJobStatus(id) {
     var popup = $('.popup-overlay');
     var content = $('.popup-editable-content');
-    var curStatus = 'In Progress';
+    var curStatus = 'Status not set';
     var statusList = [];
-
-    $.ajax('/ajax/getjobstatus/' + id).done(function(data) {
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $.ajax(baseURI + 'api/getjobstatus/' + id, { async: false }).done(function(data) {
         curStatus = data;
     });
-    $.ajax('/ajax/getstatuslist').done(function(data) {
+    $.ajax(baseURI + 'api/getstatuslist', { dataType: "json" }).done(function(data) {
         statusList = data;
-    });
 
-    content.html('<form action="/ajax/updatestatus/"' + id ' >
-    <p>
-        <option name="status">'
-        for(var item in statusList) {
-            + '<li>' + item + '</li>'
+        var str = '<span style="font-size: 18pt;">Current status</span><br />';
+        str += '<span style="font-size: 14pt;">' + curStatus + '</span><br /><br />';
+        str += '<select id="change-status" name="status">';
+        for(i = 0; i < statusList.length; i++) {
+            var s = (statusList[i].name == curStatus) ? 'selected="selected"' : '';
+            str += '<option ' + s + ' value=' + statusList[i].id + '>' + statusList[i].name + '</option>';
         }
-        + '</option>
-    </p>
-    </form>');
-    popup.css('display', 'block');
+        str += '</select>';
+        content.html(str);
+
+        $("#change-status").change(function() {
+            var name = $(this).val();
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.post(baseURI + 'api/setjobstatus/' + id, { value: name }).done(function() {
+                hideOverlay();
+                content.html('You\'re not supposed to see this.');
+                location.reload();
+            });
+        });
+        popup.css('display', 'block');
+    });
 }
 
 function hideOverlay() {

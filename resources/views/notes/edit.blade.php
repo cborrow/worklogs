2aca8f604351c94e415d812ad834ea80776f79bb
Cory Borrow <cborrow03@gmail.com>
2016-07-21 20:04:07 -0400

    Updated the edit job form.

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -5,73 +5,75 @@
 @stop
 
 @section('content')
-@if(count($errors))
-    <ul>
-    @foreach($errors->all() as $error)
-        <li class="error">{{ $error }}</li>
-    @endforeach
-    </ul>
-@endif
-<form action="/edit/{{ $job->id }}" method="post">
-{{ csrf_field() }}
-<p>
-    <label>Workorder</label>
-    <input class="medium-input" type="text" name="workorder" value="{{ $job->workorder }}" required />
-</p>
-<p>
-    <label>Customer</label>
-    <input class="medium-input" type="text" name="customer" value="{{ $job->customer }}" required />
-</p>
-<p>
-    <label>Phone #</label>
-    <input class="medium-input" type="text" name="phone" value="{{ $job->phone }}" />
-</p>
-<p>
-    <label>Status</label>
-    <select class="medium-input" name="status">
-        <option>In Progress</option>
-        <option>Parts Ordered</option>
-        <option>Waiting on Callback</option>
-        <option>Waiting on Pickup</option>
-    </select>
-</p>
-<p>
-    <label>Store</label>
-    <select class="medium-input" name="store">
-        <option>Store #001</option>
-        <option>Store #002</option>
-        <option>Store #003</option>
-    </select>
-</p>
-<p>
-    <label>Device</label>
-    <input class="medium-input" type="text" name="device" value="{{ $job->device }}" />
-</p>
-<p>
-    <label>Password</label>
-    <input class="medium-input" type="text" name="password" value="{{ $job->password }}" />
-</p>
-<p>
-    <label>Comes with</label>
-    @if($job->has_power_adapter == true)
-        <input type="checkbox" name="power_adapter" checked /> Power adapter
-    @else
-        <input type="checkbox" name="power_adapter" /> Power adapter
+<div class="container floating-box-95 no-top-pad">
+    @if(count($errors))
+        <ul>
+        @foreach($errors->all() as $error)
+            <li class="error">{{ $error }}</li>
+        @endforeach
+        </ul>
     @endif
+    <form action="/edit/{{ $job->id }}" method="post">
+    {{ csrf_field() }}
+    <p>
+        <label>Workorder</label>
+        <input class="medium-input" type="text" name="workorder" value="{{ $job->workorder }}" required />
+    </p>
+    <p>
+        <label>Customer</label>
+        <input class="medium-input" type="text" name="customer" value="{{ $job->customer }}" required />
+    </p>
+    <p>
+        <label>Phone #</label>
+        <input class="medium-input" type="text" name="phone" value="{{ $job->phone }}" />
+    </p>
+    <p>
+        <label>Status</label>
+        <select class="medium-input" name="status">
+            <option>In Progress</option>
+            <option>Parts Ordered</option>
+            <option>Waiting on Callback</option>
+            <option>Waiting on Pickup</option>
+        </select>
+    </p>
+    <p>
+        <label>Store</label>
+        <select class="medium-input" name="store">
+            <option>Store #001</option>
+            <option>Store #002</option>
+            <option>Store #003</option>
+        </select>
+    </p>
+    <p>
+        <label>Device</label>
+        <input class="medium-input" type="text" name="device" value="{{ $job->device }}" />
+    </p>
+    <p>
+        <label>Password</label>
+        <input class="medium-input" type="text" name="password" value="{{ $job->password }}" />
+    </p>
+    <p>
+        <label>Comes with</label>
+        @if($job->has_power_adapter == true)
+            <input type="checkbox" name="power_adapter" checked /> Power adapter
+        @else
+            <input type="checkbox" name="power_adapter" /> Power adapter
+        @endif
 
-    @if($job->has_carrying_case == true)
-        <input type="checkbox" name="carrying_case" checked/> Carrying case
-    @else
-        <input type="checkbox" name="carrying_case"/> Carrying case
-    @endif
-</p>
-<p>
-    <label>Notes</label>
-    <textarea class="large-input" name="notes" required>{{ $job->notes }}</textarea>
-</p>
-<p class="buttons-wide">
-    <input class="button normal" type="submit" name="submit" value="Save" />
-    <input class="button" type="submit" name="cancel" value="Cancel" />
-</p>
-</form>
+        @if($job->has_carrying_case == true)
+            <input type="checkbox" name="carrying_case" checked/> Carrying case
+        @else
+            <input type="checkbox" name="carrying_case"/> Carrying case
+        @endif
+    </p>
+    <p>
+        <label>Notes</label>
+        <textarea class="large-input" name="notes" required>{{ $job->notes }}</textarea>
+    </p>
+    <p class="buttons-wide">
+        <input class="button normal" type="submit" name="submit" value="Save" />
+        <input class="button" type="submit" name="cancel" value="Cancel" />
+    </p>
+    </form>
+</div>
 @stop

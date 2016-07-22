2c0cc0cc62e262b243f41626c642c30c164ca5ad
Cory Borrow <cborrow03@gmail.com>
2016-07-21 20:01:20 -0400

    Added methods for getColor and getName.
    Added static methods to get status color and name.

--- app/Status.php
+++ app/Status.php
@@ -9,4 +9,20 @@ class Status extends Model
     public function jobs() {
         $this->hasMany(Job::class);
     }
+
+    public static function getColor($id) {
+        $status = Status::find($id);
+
+        if($status != null)
+            return $status->color;
+        return "#fff";
+    }
+
+    public static function getName($id) {
+        $status = Status::find($id);
+
+        if($status != null)
+            return $status->name;
+        return "Unknown status";
+    }
 }

89828e27daf78b119190312060a24db4a3ac4139
Cory Borrow <cborrow03@gmail.com>
2016-07-21 20:00:18 -0400

    Added new routes.
    Added dashboard route.
    Added routes for api / ajax calls.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,12 +15,17 @@ Route::get('/', 'JobsController@index');
 Route::get('/add', 'JobsController@add');
 Route::get('/edit/{job}', 'JobsController@edit');
 Route::get('/delete', 'JobsController@delete');
+Route::get('/dashboard', 'JobsController@dashboard');
 
 Route::get('/settings', 'SettingsController@index');
 
 Route::post('/add', 'JobsController@store');
 Route::post('/edit/{job}', 'JobsController@update');
 Route::post('/delete', 'JobsController@purge');
+
+Route::get('/api/getjobstatus/{job}', 'JobsController@apigetstatus');
+Route::get('/api/getstatuslist', 'JobsController@getstatuslist');
+Route::post('/api/setjobstatus/{job}', 'JobsController@setjobstatus');
 
 Route::post('/stores/store', 'SettingsController@addstore');
 Route::post('/stores/delete/{store}', 'SettingsController@deletestore');

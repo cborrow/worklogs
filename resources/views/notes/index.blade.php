a73b8a1d248955a8febeed05b776477c32e598d0
Cory Borrow <cborrow03@gmail.com>
2016-07-21 20:04:40 -0400

    Changed create button, added search, updated job lists.

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,7 +1,8 @@
 @extends('master')
 
 @section('header')
-<a href="/add" class="button normal">Create</a>
+<a href="/add" class="button normal" id="create-button">Create</a>
+<input type="text" name="q" class="medium-input" placeholder="Start typing to search" />
 <span class="right semi-bold">
     <label>Filter Jobs</lable>
     <select name="job_filter">
@@ -16,29 +17,35 @@
 @stop
 
 @section('content')
-<table cellspacing="0" cellpadding="0">
-    <thead>
-        <td class="col-10">Workorder</td>
-        <td class="col-20">Customer</td>
-        <td class="col-25">Status</td>
-        <td class="col-25">Last Update</td>
-        <td class="col-15"></td>
-    </thead>
-    <tbody>
-        @foreach ($jobs as $job)
-        <tr>
-            <td>{{ $job->workorder }}</td>
-            <td>{{ $job->customer }}</td>
-            <td><span class="status {{ \App\Job::getStatusClass($job->status_id) }}"><a href="javascript:changeJobStatus('{{ $job->id }}');">{{ \App\Job::getStatusName($job->status_id) }}</a></span></td>
-            <td>{{ $job->updated_at }}</td>
-            <td><span class="rounded-button">
-                <a href="javascript:showPassword('{{ $job->password }}');" title="Show Password"><i class="fa fa-key" aria-hidden="true"></i></a>
-                <a href="#" title="Edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                <a href="#" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
-                <a href="#" title="Close"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
-            </span></td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="container floating-box-95 no-top-pad">
+    <table cellspacing="0" cellpadding="0">
+        <thead>
+            <td>Workorder</td>
+            <td>Customer</td>
+            <td>Store</td>
+            <td>Status</td>
+            <td>Last Update</td>
+            <td class="col-10"></td>
+        </thead>
+        <tbody>
+            @foreach ($jobs as $job)
+            <tr>
+                <td>{{ $job->workorder }}</td>
+                <td>{{ $job->customer }}</td>
+                <td>{{ \App\Store::getName($job->store_id) }}</td>
+                <td><span class="status" style="background: {{ \App\Job::getStatusColor($job->status_id) }}; border: {{ \App\Job::getStatusColor($job->status_id) }} solid 1px;">
+                    <a href="javascript:changeJobStatus('{{ $job->id }}');">{{ \App\Job::getStatusName($job->status_id) }}</a>
+                </span></td>
+                <td>{{ $job->updated_at }}</td>
+                <td><span class="rounded-button">
+                    <a href="javascript:showPassword('{{ $job->password }}');" title="Show Password"><i class="fa fa-key" aria-hidden="true"></i></a>
+                    <a href="#" title="Edit"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                    <a href="#" title="Delete"><i class="fa fa-trash-o" aria-hidden="true"></i></a>
+                    <a href="#" title="Close"><i class="fa fa-check-square-o" aria-hidden="true"></i></a>
+                </span></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @stop
